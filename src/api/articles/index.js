14d079b231a642c0763ca2eb96e09015643a336e
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-03-15 15:12:48 +0100

    added comments endpoints

--- src/api/articles/index.js
+++ src/api/articles/index.js
@@ -101,43 +101,138 @@ articlesRouter.delete("/:articleId", async (req, res, next) => {
   }
 });
 
+// ----------------------------COMMENTS ENDPOINTS---------------------------------------
 articlesRouter.post("/:articleId/comments", async (req, res, next) => {
   try {
     const newComment = {
       ...req.body,
-      id: uniqid(),
-      createdAt: new Date(),
-      updatedAt: new Date(),
+      dateOfPosting: new Date(),
     };
-    const articleArray = await getArticles();
-    const index = articleArray.findIndex(
-      (article) => article.id === req.params.articleId
-    );
-    if (index !== -1) {
-      const oldArticle = articleArray[index];
-      const updatedArticle = {
-        ...oldArticle,
-        comments: [...oldArticle.comments, newComment],
-      };
-      articleArray[index] = updatedArticle;
-      await writeArticles(articleArray);
-
-      res.status(201).send({ id: newComment.id, newComment });
+    const updatedArticle = await ArticlesModel.findByIdAndUpdate(
+      req.params.articleId, // WHO
+      { $push: { comments: newComment } }, // HOW
+      { new: true, runValidators: true }
+    ); // OPTIONS
+    if (updatedArticle) {
+      res.status(201).send(newComment);
+    } else {
+      next(
+        createHttpError(
+          404,
+          `Article with id ${req.params.articleId} not found!`
+        )
+      );
     }
   } catch (error) {
     console.log(error);
   }
 });
+
 articlesRouter.get("/:articleId/comments", async (req, res, next) => {
   try {
-    const articleArray = await getArticles();
-    const index = articleArray.findIndex(
-      (article) => article.id === req.params.articleId
-    );
-
-    res.send(articleArray[index].comments);
+    const targetArticle = await ArticlesModel.findById(req.params.articleId);
+    const comments = targetArticle.comments;
+    if (targetArticle) {
+      res.status(200).send(comments);
+    } else {
+      next(
+        createHttpError(
+          404,
+          `Article with id ${req.params.articleId} not found!`
+        )
+      );
+    }
   } catch (error) {
     next(error);
   }
 });
+
+articlesRouter.get(
+  "/:articleId/comments/:commentsId",
+  async (req, res, next) => {
+    try {
+      const targetArticle = await ArticlesModel.findById(req.params.articleId);
+      const targetedComment = targetArticle.comments.find(
+        (comment) => comment._id.toString() === req.params.commentsId
+      );
+
+      if (targetArticle) {
+      } else {
+        createHttpError(
+          404,
+          `Article with id ${req.params.articleId} not found!`
+        );
+      }
+      if (targetedComment) {
+        res.send(targetedComment);
+      } else {
+        createHttpError(
+          404,
+          `Comment with id ${req.params.commentsId} not found!`
+        );
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+articlesRouter.put(
+  "/:articleId/comments/:commentsId",
+  async (req, res, next) => {
+    try {
+      const targetArticle = await ArticlesModel.findById(req.params.articleId);
+      if (targetArticle) {
+        let index = targetArticle.comments.findIndex(
+          (comment) => comment._id.toString() === req.params.commentsId
+        );
+        if (index !== -1) {
+          targetArticle.comments[index] = {
+            ...targetArticle.comments[index].toObject(),
+            ...req.body,
+            dateOfUpdate: new Date(),
+          };
+          console.log(targetArticle.comments[index]);
+          await targetArticle.save();
+          res.send(targetArticle.comments[index]);
+        } else {
+          createHttpError(
+            404,
+            `Comment with id ${req.params.articleId} not found!`
+          );
+        }
+      } else {
+        createHttpError(
+          404,
+          `Article with id ${req.params.commentsId} not found!`
+        );
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+articlesRouter.delete(
+  "/:articleId/comments/:commentsId",
+  async (req, res, next) => {
+    try {
+      const updatedArticle = await ArticlesModel.findByIdAndUpdate(
+        req.params.articleId, // WHO
+        { $pull: { comments: { _id: req.params.commentsId } } }, // HOW
+        { new: true, runValidators: true } // OPTIONS
+      );
+
+      if (updatedArticle) {
+        res.send(updatedArticle);
+      } else {
+        createHttpError(
+          404,
+          `Article with id ${req.params.commentsId} not found!`
+        );
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 export default articlesRouter;
--- src/api/articles/model.js
+++ src/api/articles/model.js
@@ -9,13 +9,21 @@ const articleSchema = new Schema(
     cover: { type: String, required: true },
     readTime: {
       value: { type: Number },
-      unit: { type: String },
+      unit: { type: String, enum: ["minutes", "seconds", "hours"] },
     },
     author: {
       name: { type: String },
       avatar: { type: String },
     },
     content: { type: String, required: true },
+    comments: [
+      {
+        title: String,
+        text: String,
+        dateOfPosting: Date,
+        dateOfUpdate: Date,
+      },
+    ],
   },
   {
     timestamps: true,
